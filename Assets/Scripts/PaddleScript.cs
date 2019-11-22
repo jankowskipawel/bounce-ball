62ae5382dc079c41f7890e3b2be1aac277525319
jankowskipawel <->
2019-11-22 19:29:50 +0100

    Add moving paddle and constrain it

---
+++ Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+    public float maxX; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+
+        if (x == 0)
+        {
+            Stop();
+        }
+
+        if (x > 0)
+        {
+            MoveRight();
+        }
+
+        if (x < 0)
+        {
+            MoveLeft();
+        }
+
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, -maxX, maxX);
+        transform.position = pos;
+    }
+
+    void MoveLeft()
+    {
+        rb.velocity = new Vector2(-speed, 0);
+    }
+
+    void MoveRight()
+    {
+        rb.velocity = new Vector2(speed, 0);
+    }
+
+    void Stop()
+    {
+        rb.velocity = Vector2.zero;
+    }
+
+    //-15.75
+}
