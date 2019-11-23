1b9536025c8a8756218d7c3f915e915c1bcb0f31
Pawel MacOS <paweljankowski246@gmail.com>
2019-11-23 02:41:03 +0100

    Fix ability to press space(add force to ball) multiple times

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -7,7 +7,7 @@ public class BallScript : MonoBehaviour
 
     public Rigidbody2D rb;
     public float ballForce;
-
+    bool gameStarted;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +16,12 @@ public class BallScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !gameStarted)
         {
             transform.SetParent(null);
             rb.simulated = true;
             rb.AddForce(new Vector2(ballForce, ballForce));
-
+            gameStarted = true;
         }
     }
 }
